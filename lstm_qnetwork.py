5f8c4ef68e0d055bf2890f3d967f6ab0dd97c28f
Yash Shah <yashshah00@outlook.com>
2025-03-01 17:50:38 +0000

    Added LSTM network and training script.

---
+++ lstm_qnetwork.py
@@ -0,0 +1,59 @@
+import torch.nn.functional as F
+from einops import rearrange
+from skrl.models.torch import Model
+from torch import nn
+import torch
+
+
+class LSTMQNetwork(Model):
+    def __init__(self, observation_space, action_space, device, d_model=128):
+        super().__init__(observation_space, action_space, device)
+
+        # Flattened one-hot encoded observation
+        self.sequence_length = observation_space[0].shape[0]
+        self.observation_dim = observation_space[0].shape[0] * 4 + observation_space[1].shape[0] * 2
+        self.num_actions = action_space.n
+        self.num_layers = 2
+        self.d_model = d_model
+
+        self.lstm = nn.LSTM(input_size=6, hidden_size=d_model, num_layers=self.num_layers, batch_first=True)
+        self.fc1 = nn.Linear(d_model, d_model)
+        self.fc2 = nn.Linear(d_model, self.num_actions)
+
+    def compute(self, inputs, role):
+        observation_actions, observation_sequence = inputs["states"][..., :self.sequence_length], inputs["states"][..., self.sequence_length:]
+
+        one_hot_actions = F.one_hot(observation_actions.long(), num_classes=4)
+        one_hot_sequence = F.one_hot(observation_sequence.long(), num_classes=2)
+        one_hot_input = torch.cat([one_hot_actions, one_hot_sequence], dim=-1).float()
+
+        batch_size = one_hot_actions.shape[0]
+
+        h0 = torch.zeros(self.num_layers, batch_size, self.d_model).float()
+        c0 = torch.zeros(self.num_layers, batch_size, self.d_model).float()
+        out, _ = self.lstm(one_hot_input, (h0, c0))
+        x = out[:, -1, :]
+
+        x = F.relu(self.fc1(x))
+        q_values = self.fc2(x)
+
+        return q_values, {}
+
+    def act(self, inputs, role):
+        observation_actions, observation_sequence = inputs["states"][..., :self.sequence_length], inputs["states"][..., self.sequence_length:]
+
+        one_hot_actions = F.one_hot(observation_actions.long(), num_classes=4)
+        one_hot_sequence = F.one_hot(observation_sequence.long(), num_classes=2)
+        one_hot_input = torch.cat([one_hot_actions, one_hot_sequence], dim=-1).float()
+
+        batch_size = one_hot_actions.shape[0]
+
+        h0 = torch.zeros(self.num_layers, batch_size, self.d_model)
+        c0 = torch.zeros(self.num_layers, batch_size, self.d_model)
+        out, _ = self.lstm(one_hot_input, (h0, c0))
+        x = out[:, -1, :]
+
+        x = F.relu(self.fc1(x))
+        q_values = self.fc2(x)
+
+        return q_values, None, {}
---
+++ lstm_train.py
@@ -0,0 +1,81 @@
+import gymnasium as gym
+# import the skrl components to build the RL system
+from skrl.agents.torch.dqn import DQN, DQN_DEFAULT_CONFIG
+from skrl.envs.wrappers.torch import wrap_env
+from skrl.memories.torch import RandomMemory
+from skrl.trainers.torch import SequentialTrainer
+from skrl.utils import set_seed
+from skrl.utils.model_instantiators.torch import Shape, deterministic_model
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+import envs
+from lstm_qnetwork import LSTMQNetwork
+
+# seed for reproducibility
+set_seed(42)  # e.g. `set_seed(42)` for fixed seed
+
+
+# load and wrap the gymnasium environment.
+# note: the environment version may change depending on the gymnasium version
+# seq = "HHHPHPHHPPP"
+# seq = "PPHPPHHPPHHPPPPPHHHHHHHHHHPPPPPPHHPPHHPPHPPHHHHH"
+seq = "PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP"
+env = gym.make("HPEnv_v0", seq=seq)
+env = wrap_env(env)
+
+device = env.device
+
+
+# instantiate a memory as experience replay
+memory = RandomMemory(memory_size=50000, num_envs=env.num_envs, device=device)
+
+
+# instantiate the agent's models (function approximators) using the model instantiator utility.
+# DQN requires 2 models, visit its documentation for more details
+# https://skrl.readthedocs.io/en/latest/api/agents/dqn.html#models
+models = {}
+models["q_network"] = LSTMQNetwork(observation_space=env.observation_space,
+                                  action_space=env.action_space,
+                                  device=device)
+models["target_q_network"] = LSTMQNetwork(observation_space=env.observation_space,
+                                         action_space=env.action_space,
+                                         device=device)
+
+# initialize models' lazy modules
+for role, model in models.items():
+    model.init_state_dict(role)
+
+# initialize models' parameters (weights and biases)
+for model in models.values():
+    model.init_parameters(method_name="normal_", mean=0.0, std=0.1)
+
+
+# configure and instantiate the agent (visit its documentation to see all the options)
+# https://skrl.readthedocs.io/en/latest/api/agents/dqn.html#configuration-and-hyperparameters
+cfg = DQN_DEFAULT_CONFIG.copy()
+cfg["batch_size"] = 200
+cfg["learning_starts"] = 5000
+cfg["exploration"]["final_epsilon"] = 0.01
+cfg["exploration"]["timesteps"] = 15000
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 1000
+cfg["experiment"]["checkpoint_interval"] = 5000
+cfg["learning_rate"] = 0.0005
+# cfg["learning_rate_scheduler"] = CosineAnnealingLR
+# cfg["learning_rate_scheduler_kwargs"] = {'T_max': 100}
+cfg["experiment"]["directory"] = "runs/lstm_36_mer/"
+
+agent = DQN(models=models,
+            memory=memory,
+            cfg=cfg,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=device)
+
+
+# configure and instantiate the RL trainer
+cfg_trainer = {"timesteps": 500000, "headless": True}
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=[agent])
+
+# start training
+trainer.train()
