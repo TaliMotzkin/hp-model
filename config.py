cde3c47eb0ee489134002ed1a7b61d3bb78eef91
TaliMotzkin <104077339+TaliMotzkin@users.noreply.github.com>
2025-03-15 09:51:53 +0000

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,17 @@ def parse_args():
     parser.add_argument("--seq", type=str, default='HHHPPHPHPHPPHPHPHPPH')
     parser.add_argument("--agent_choice", type=str, default='dqn')
     parser.add_argument("--buffer", type=str, default='random')
-    parser.add_argument("--update_timestep", type=int, default=300)
+    parser.add_argument("--update_timestep", type=int, default=10)
     parser.add_argument("--K_epochs", type=int, default=150)
     parser.add_argument("--use_curriculum", type=int, default=0)
     parser.add_argument("--revisit_probability", type=float, default=0.2)
     parser.add_argument("--saved_path_model", type=str, default="")
     parser.add_argument("--seq_list", type=str, default="data/hp_sequences_dataset.pkl")
-
+    parser.add_argument("--start_learning", type=int, default=0)
+    parser.add_argument("--stop_learning", type=int, default=4)
+    parser.add_argument("--pre_trained_model", type=str, default="")
+    parser.add_argument("--pre_trained", type=int, default=0)
+    parser.add_argument("--progress_threshold", type=float, default=0.5)
 
     args = parser.parse_args()
     return args
