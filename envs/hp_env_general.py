3ddcdcddac8ad93a9447977a171b4b6a31841e2d
TaliMotzkin <104077339+TaliMotzkin@users.noreply.github.com>
2025-03-22 11:52:17 +0000

    Update hp_env_general.py

--- envs/hp_env_general.py
+++ envs/hp_env_general.py
@@ -28,7 +28,7 @@ class HPEnvGeneral(gym.Env):
             elif character == 'P':
                 observation_sequence.append(1)
         self.observation_sequence = np.array(observation_sequence, dtype=np.int64)
-
+        self.prev_reward = 0
         self.reset()
 
         if len(self.seq) <= 2:
@@ -129,8 +129,26 @@ class HPEnvGeneral(gym.Env):
                 if (nx, ny) in h_positions and self.state.index((nx, ny)) not in {self.state.index((x, y)) - 1, self.state.index((x, y)) + 1}:
                     num_contacts += 1
         total_reward = num_contacts / 2  # Each pair is counted twice
-        normlized_reward = total_reward / self.maximal_reward
+        normlized_reward = total_reward 
         return normlized_reward
+        # num_contacts = 0.
+        # if self.terminated:
+        #     normlized_reward = self.prev_reward
+        # else:
+        #     h_positions = {pos for idx, pos in enumerate(self.state) if self.seq[idx] == 'H'}
+
+        #     # Check adjacent H-H interactions
+        #     for x, y in h_positions:
+        #         neighbors = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        #         for nx, ny in neighbors:
+        #             # Second condition checks if non-sequential
+        #             if (nx, ny) in h_positions and self.state.index((nx, ny)) not in {self.state.index((x, y)) - 1,
+        #                                                                               self.state.index((x, y)) + 1}:
+        #                 num_contacts += 1
+        #     total_reward = num_contacts / 2  # Each pair is counted twice
+        #     normlized_reward = total_reward
+        #     self.prev_reward = normlized_reward
+        # return normlized_reward
 
 
     def observe(self):
